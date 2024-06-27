be6fab60675137327c476361593a2fc83c4eca8e
Mohit <mohit.joshi@capanicus.com>
2024-06-27 11:32:37 +0530

    server cors origin changes

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [`${process.env.SITE_URL}:${process.env.SITE_PORT}`],
+    origin: [process.env.MODE === 'DEV' ? `${process.env.SITE_URL}:${process.env.SITE_PORT}` : `${process.env.SITE_URL}`],
   },
 });
 
