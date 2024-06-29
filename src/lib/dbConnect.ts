bf5ee5eaf6fa49f4812816b9eb600103bef4c9f3
Mohit <mohit.joshi@capanicus.com>
2024-06-29 23:11:50 +0530

    db connect function with mongo uri setup

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+export const dbConnect = async (): Promise<void> => {
+  if (connection?.isConnected) {
+    console.log("Already Connected to Database");
+    return;
+  }
+  try {
+    if (!process.env.MONGO_URI) {
+      console.log("Database uri not found");
+      process.exit(1);
+    }
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Successfully Connected to Database");
+  } catch (error) {
+    console.log("Error while Connecting to Database", { error });
+    process.exit(1);
+  }
+};
