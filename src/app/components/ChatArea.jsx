68fa403c4d6fba09aa764b4f12d330ebe2cc6511
Mohit Joshi <withmohitjoshi@gmail.com>
2024-10-02 20:44:30 +0530

    changes

--- src/app/components/ChatArea.jsx
+++ src/app/components/ChatArea.jsx
@@ -32,16 +32,10 @@ function Header() {
 function Body() {
   return (
     <div className="flex-1 overflow-y-auto p-4 space-y-4">
-      <Message sender="them" content="Hey, how's it going?" time="10:30 AM" />
-      <Message
-        sender="me"
-        content="Hello! I'm doing well. How about you?"
-        time="10:35 AM"
-      />
       <Message
         sender="them"
-        content="I'm good too. Just working on some new designs."
-        time="10:40 AM"
+        content="Chat feature is in under construction"
+        time="12:00 PM"
       />
     </div>
   );
